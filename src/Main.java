d10c75be1556011abf0a91f0b9fe98885b54b9d9
Gabriel <gabrielsiodoni@hotmail.com>
2022-02-21 17:21:05 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição Curso Java");
+        curso1.setCargaHoraria(120);
+
+        Curso curso2 = new Curso();
+        curso1.setTitulo("Curso JavaScript");
+        curso1.setDescricao("Descrição Curso JavaScript");
+        curso1.setCargaHoraria(120);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição Mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
